9fd7266fe32abb28be180961b1e1f11115eee51d
Rob Nahf <rnahf@epscor.unm.edu>
2014-01-08 08:29:00 +0000

    refactored comparison methods from d1_audit_java into d1_common_java TypeCompareUtil class for better utilization, and especially use by d1_cn_audit.

---
+++ src/main/java/org/dataone/service/types/TypeCompareUtil.java
@@ -0,0 +1,301 @@
+/**
+ * This work was created by participants in the DataONE project, and is
+ * jointly copyrighted by participating institutions in DataONE. For 
+ * more information on DataONE, see our web site at http://dataone.org.
+ *
+ *   Copyright ${year}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ * 
+ * $Id$
+ */
+
+package org.dataone.service.types;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.apache.log4j.Logger;
+import org.dataone.service.types.v1.AccessRule;
+import org.dataone.service.types.v1.Subject;
+import org.dataone.service.types.v1.SystemMetadata;
+
+
+/**
+ * This utility class provides an efficient way to test bean-style data objects
+ * (of the same type) for differences in property values.
+ * 
+ * @author rnahf
+ *
+ */
+public class TypeCompareUtil {
+	
+	static Logger logger = Logger.getLogger(TypeCompareUtil.class.getName());
+
+	
+	/**
+	 * Reports the properties of a systemMetadata object, one per line in xpath 
+	 * fashion.  the values of each property follow in subsequent columns per line.
+	 * 
+	 * @param sysmeta1
+	 * @param sysmeta2
+	 * @return - the report as a List<String> of differences
+	 */
+	public static List<String> compareSystemMetadata(SystemMetadata sysmeta1, SystemMetadata sysmeta2) {
+		
+		LinkedHashMap<String,Object> labeledSysmeta = new LinkedHashMap<String,Object>();
+
+		labeledSysmeta.put("sysmeta 1", sysmeta1);
+		labeledSysmeta.put("sysmeta 2", sysmeta2);
+		
+		return compareD1ServiceType(labeledSysmeta,true);
+	}
+	
+	/**
+	 * This method will compare any number of dataone service-type objects of 
+	 * the same type to each other and return a report of every field where 
+	 * there is a difference. Multi-valued fields are sorted by default to minimize 
+	 * reporting of meaningless differences, but with the possible side-effect 
+	 * of shifting elements out of alignment and reporting more rows than 
+	 * differences when there are differences.
+	 * 
+	 * @param d1ServiceTypeMap - a map of dataone service-type objects
+	 * @param sortArrays - Boolean: if set to false, will not sort the arrays prior
+	 *        to comparison.
+	 * @return - a table of fields where there are differences
+	 */
+    public static List<String> compareD1ServiceType(Map<String,Object> d1ServiceTypeMap, Boolean sortArrays) {
+    	
+    	if (sortArrays == null) {
+    		sortArrays = true;
+    	}
+    	
+    	List<String> report = new LinkedList<String>();
+		Map<String,Object> d1TypePropMap = new HashMap<String,Object>();
+		Set<String> propKeys = new TreeSet<String>();
+		
+		for (String label : d1ServiceTypeMap.keySet()) {
+			Map<String,String> smdProps = TypeCompareUtil.getD1SubtypesListing("", d1ServiceTypeMap.get(label), sortArrays);
+			propKeys.addAll(smdProps.keySet());
+			d1TypePropMap.put(label, smdProps);
+		}
+    	
+		
+		Set<String> nodeKeys = d1TypePropMap.keySet();
+		StringBuffer sb = new StringBuffer();
+		sb.append("property field");
+		for (String key: nodeKeys) {
+			sb.append("\t" + key);
+		}
+		report.add(sb.toString());
+		
+		for (String prop: propKeys) {
+			sb = new StringBuffer();
+			sb.append(prop);
+			boolean difference = false;
+			String firstValue = null;
+			for (String nodeKey : nodeKeys) {
+				sb.append("\t");
+				if (d1TypePropMap.get(nodeKey) instanceof LinkedHashMap) {
+					String value = ((LinkedHashMap<String,String>)d1TypePropMap.get(nodeKey)).get(prop);
+					sb.append(value);
+					if (firstValue == null) {
+						firstValue = value;
+					} else if ( !firstValue.equals(value) ) {
+						difference = true;
+					}
+				} else {
+					sb.append(d1TypePropMap.get(nodeKey));
+					difference = true;
+				}
+			}
+			if (difference) {
+				report.add(sb.toString());
+			}
+		}
+		if (report.size() == 1) {
+			report.clear();
+			report.add("OK");
+		}
+		return report;
+	}
+	
+	
+	
+	
+	/**
+	 * A recursive method that returns a map of key value pairs that correspond 
+	 * to the property paths (keys) and values of the object passed in.
+	 * It depends on bean-style getter methods to find the properties of the 
+	 * object.
+	 * It will sort subtypes that are arrays when order doesn't matter.  For a strict
+	 * non-sorting listing, use getD1SubtypesListing(String path, Object o, boolean sortArrays)
+	 * and set sortArrays to false.
+	 * @param o - the dataone object
+	 * @return using LinkedHashMap to ensure order read equals order listed
+	 */
+	public static LinkedHashMap<String,String> getD1SubtypesListing(Object o) {
+		return getD1SubtypesListing(o.getClass().getSimpleName(), o, true);
+		
+	}
+	
+	/**
+	 * A recursive method that returns a map of key value pairs that correspond 
+	 * to the property paths (keys) and values of the object passed in.
+	 * It depends on bean-style getter methods to find the properties of the 
+	 * object.
+	 * Even with sortArrays = true, the Replica list will not be sorted 
+	 * because order is considered important. 
+	 * @param path
+	 * @param o - the dataone object
+	 * @param sortArrays - true will sort array subtypes so they can be logically compared
+	 * @return using LinkedHashMap to ensure order read equals order listed
+	 */
+	public static LinkedHashMap<String,String> getD1SubtypesListing(String path, Object o, boolean sortArrays) {
+		
+		LinkedHashMap<String,String> results = new LinkedHashMap<String,String>();
+		if (o == null) {
+			results.put(path, "null");
+		}
+		else {
+
+			Class cl = o.getClass();
+			logger.debug("listing for: " + path + ": " + cl.getName());
+			// only dataone service types are dealt with here, because we know 
+			// they follow the bean convention of set/get methods to access properties
+			if (cl.getPackage().getName().startsWith("org.dataone.service.types")) {
+				Method[] methods = o.getClass().getMethods();
+				for (Method m : methods) {
+					// if a get method that isn't "getClass", and doesn't take
+					// a parameter (avoids list-associated methods) OR is an
+					// enumeration (that uses xmlValue to get the value...
+					if ((m.getName().startsWith("get") && !m.getName().equals("getClass")
+							&& m.getParameterTypes().length == 0)
+							|| m.getName().equals("xmlValue")) {
+						try {
+							Object p = m.invoke(o, (Object[]) null);
+							Map<String,String> subresults = getD1SubtypesListing(
+									path + "/" + m.getName().replace("get", ""),
+									p,
+									sortArrays);
+							results.putAll(subresults);
+						} catch (IllegalArgumentException e) {
+							logger.error(m.getName(),e);
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (IllegalAccessException e) {
+							logger.error(m.getName(),e);
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (InvocationTargetException e) {
+							logger.error(m.getName(),e);
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					} 
+				}
+			} 
+			// otherwise, we are at a more basic datatype (String, etc)
+			else {
+				if (o instanceof String) {
+					results.put(path, (String) o);
+				}
+				else if (o instanceof Integer) {
+					results.put(path, ((Integer)o).toString());
+				}
+				else if (o instanceof Date) {
+					results.put(path, ((Date)o).toString());
+				}
+				else if (o instanceof BigInteger) {
+					results.put(path, ((BigInteger)o).toString());
+				}
+				else if (o instanceof Boolean) {
+					results.put(path, ((Boolean)o).toString());
+				} 
+				else if (o instanceof ArrayList) {	
+					ArrayList<?> list = ((ArrayList<?>) o);
+					if ( !list.isEmpty() ) {
+						if (sortArrays) {
+							logger.debug("arrayList size: " + list.size() );
+							// many of the dataone simpletypes are sortable (implement Comparable)
+							if (list.get(0) instanceof Comparable) {
+								// permissions, Subjects, and Identifiers are Comparable, so sort and go
+								Collections.sort((List<Comparable>)list);
+							} 
+							// ADD COMPARATORS HERE for subtype properties that are
+							// lists of non-Comparable dataone service-types
+							// if those need to be sorted (where list order doesn't 
+							// affect behavior)
+							
+							// The order of AccessRules in an AccessPolicy doesn't
+							// affect behavior, so should be made Comparable so 
+							// equivalence can be tested
+							else if (list.get(0) instanceof AccessRule) {
+								Collections.sort((List<AccessRule>)list, 
+								    new Comparator<AccessRule>() {
+									    public int compare(AccessRule ar1, AccessRule ar2) {
+									    	
+									    	// only using subjectlist for sorting
+									    	// because it's highly unlikely that 
+									    	// that the same subject list will be
+									    	// listed twice under separate AccessRules
+									    	StringBuffer ar1SB = new StringBuffer();
+									    	Collections.sort(ar1.getSubjectList());
+									    	for(Subject s : ar1.getSubjectList()) {
+									    		ar1SB.append(s.getValue());
+									    	}
+									    	String ar1Subjects = ar1SB.toString();
+									    	
+									    	StringBuffer ar2SB = new StringBuffer();
+									    	Collections.sort(ar2.getSubjectList());
+									    	for(Subject s : ar2.getSubjectList()) {
+									    		ar2SB.append(s.getValue());
+									    	}
+									    	String ar2Subjects = ar2SB.toString();
+
+									    	return ar1Subjects.compareTo(ar2Subjects);
+									    }
+								    }
+								);
+							}
+							else {
+							    ;	// don't sort...
+							}
+						} 
+						for (int j = 0; j < list.size(); j++) {
+							results.putAll(getD1SubtypesListing( (path + "/" + j),
+						             list.get(j),sortArrays));
+						}
+					} 
+					else {
+						results.put(path,"{empty}");
+					}
+				}
+			}
+		}
+		logger.debug("(return)");
+		return results;
+	}
+}
---
+++ src/test/java/org/dataone/service/types/TypeCompareUtilTestCase.java
@@ -0,0 +1,194 @@
+package org.dataone.service.types;
+
+import static org.junit.Assert.*;
+
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.dataone.service.types.v1.AccessPolicy;
+import org.dataone.service.types.v1.Identifier;
+import org.dataone.service.types.v1.NodeReference;
+import org.dataone.service.types.v1.ObjectFormatIdentifier;
+import org.dataone.service.types.v1.Permission;
+import org.dataone.service.types.v1.Replica;
+import org.dataone.service.types.v1.ReplicationPolicy;
+import org.dataone.service.types.v1.ReplicationStatus;
+import org.dataone.service.types.v1.Subject;
+import org.dataone.service.types.v1.SystemMetadata;
+import org.dataone.service.types.v1.util.AccessUtil;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TypeCompareUtilTestCase {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+//	@Test
+	public final void testCompareD1ServiceType() {
+		fail("Not yet implemented");
+	}
+
+
+	@Test
+	public void testGetD1SubtypesListing() {
+		SystemMetadata smd = new SystemMetadata();
+		smd.setIdentifier(buildIdentifier("myID"));
+		smd.setFormatId(buildFormatIdentifier("myFormatID"));
+		smd.setDateUploaded(new Date());
+		smd.setAuthoritativeMemberNode(buildNodeReference("urn:node:myNode"));
+		smd.setDateSysMetadataModified(new Date());
+		smd.setRightsHolder(buildSubject("me"));
+		smd.setSerialVersion(new BigInteger("12345"));
+		Replica r = new Replica();
+		r.setReplicaMemberNode(buildNodeReference("urn:node:Xanother"));
+		r.setReplicationStatus(ReplicationStatus.REQUESTED);
+		r.setReplicaVerified(new Date());
+		smd.addReplica(r);
+		r = new Replica();
+		r.setReplicationStatus(ReplicationStatus.COMPLETED);
+		r.setReplicaVerified(new Date());
+		r.setReplicaMemberNode(buildNodeReference("urn:node:ONEmore"));
+		smd.addReplica(r);
+		
+		
+		AccessPolicy ap = new AccessPolicy();
+		ap.addAllow(AccessUtil.createAccessRule(
+				new String[]{"you","me"},
+				new Permission[]{Permission.READ, Permission.WRITE, Permission.CHANGE_PERMISSION})
+				);
+		ap.addAllow(AccessUtil.createAccessRule(
+				new String[]{"him","her"},
+				new Permission[]{Permission.WRITE, Permission.READ})
+				);
+		smd.setAccessPolicy(ap);
+		
+		ReplicationPolicy rp = new ReplicationPolicy();
+		rp.setNumberReplicas(5);
+		rp.setReplicationAllowed(true);
+		rp.addBlockedMemberNode(buildNodeReference("urn:node:crazyUncleMN"));
+		rp.addPreferredMemberNode(buildNodeReference("urn:node:cousinMN"));
+		rp.addPreferredMemberNode(buildNodeReference("urn:node:brotherMN"));
+		smd.setReplicationPolicy(rp);
+		
+		
+//		List<String> report = getSubtypes("",smd);
+//		for (String s: report) {
+//			System.out.println(s);
+//		}
+		Map<String,String> report = TypeCompareUtil.getD1SubtypesListing("",smd,true);
+		for (String s: report.keySet()) {
+			System.out.println(s + "\t" + report.get(s)); 
+		}
+	}
+	
+	
+	@Test
+	public void testCompareSysMeta() {
+		SystemMetadata sysMeta1 = createStandardSysmeta();
+		SystemMetadata sysMeta2 = createStandardSysmeta();
+		
+		List<String> report = TypeCompareUtil.compareSystemMetadata(sysMeta1, sysMeta2);
+		
+		assertEquals("If they are the same, then it should be a one-line report",report.size(),1);
+		assertEquals("If they are the same, then should get 'OK'",report.get(0),"OK");
+		
+		AccessPolicy ap = sysMeta2.getAccessPolicy();
+		AccessPolicy apNew = new AccessPolicy();
+		apNew.addAllow(ap.getAllow(1));
+		apNew.addAllow(ap.getAllow(0));
+		sysMeta2.setAccessPolicy(apNew);
+		
+		report = TypeCompareUtil.compareSystemMetadata(sysMeta1, sysMeta2);
+		for (String line : report) {
+			System.out.println(line);
+		}
+		System.out.println("");
+		assertEquals("If they are the same, then it should be a one-line report",report.size(),1);
+		assertEquals("If they are the same, then should get 'OK'",report.get(0),"OK");
+		
+		
+		sysMeta2.getReplicationPolicy().setNumberReplicas(2);
+		report = TypeCompareUtil.compareSystemMetadata(sysMeta1, sysMeta2);
+		for (String line : report) {
+			System.out.println(line);
+		}
+		assertTrue("Should have at least 2 rows", report.size() >= 2);
+		assertTrue("Should have a difference in the number of replicas",
+				report.get(1).contains("NumberReplicas"));	
+	}
+
+	
+	
+	private SystemMetadata createStandardSysmeta() {
+		SystemMetadata smd = new SystemMetadata();
+		smd.setIdentifier(buildIdentifier("myID"));
+		smd.setFormatId(buildFormatIdentifier("myFormatID"));
+		smd.setDateUploaded(new Date());
+		smd.setAuthoritativeMemberNode(buildNodeReference("urn:node:myNode"));
+		smd.setDateSysMetadataModified(new Date());
+		smd.setRightsHolder(buildSubject("me"));
+		smd.setSerialVersion(new BigInteger("12345"));
+		Replica r = new Replica();
+		r.setReplicaMemberNode(buildNodeReference("urn:node:Xanother"));
+		r.setReplicationStatus(ReplicationStatus.REQUESTED);
+		r.setReplicaVerified(new Date());
+		smd.addReplica(r);
+		r = new Replica();
+		r.setReplicationStatus(ReplicationStatus.COMPLETED);
+		r.setReplicaVerified(new Date());
+		r.setReplicaMemberNode(buildNodeReference("urn:node:ONEmore"));
+		smd.addReplica(r);
+		
+		
+		AccessPolicy ap = new AccessPolicy();
+		ap.addAllow(AccessUtil.createAccessRule(
+				new String[]{"you","me"},
+				new Permission[]{Permission.READ, Permission.WRITE, Permission.CHANGE_PERMISSION})
+				);
+		ap.addAllow(AccessUtil.createAccessRule(
+				new String[]{"him","her"},
+				new Permission[]{Permission.WRITE, Permission.READ})
+				);
+		smd.setAccessPolicy(ap);
+		
+		ReplicationPolicy rp = new ReplicationPolicy();
+		rp.setNumberReplicas(5);
+		rp.setReplicationAllowed(true);
+		rp.addBlockedMemberNode(buildNodeReference("urn:node:crazyUncleMN"));
+		rp.addPreferredMemberNode(buildNodeReference("urn:node:cousinMN"));
+		rp.addPreferredMemberNode(buildNodeReference("urn:node:brotherMN"));
+		smd.setReplicationPolicy(rp);
+		
+		return smd;
+	}
+	
+	private static Identifier buildIdentifier(String value) {
+		Identifier id = new Identifier();
+		id.setValue(value);
+		return id;
+	}
+	
+	private static Subject buildSubject(String value) {
+		Subject subject = new Subject();
+		subject.setValue(value);
+		return subject;
+	}
+	
+	private static ObjectFormatIdentifier buildFormatIdentifier(String value) {
+		ObjectFormatIdentifier id = new ObjectFormatIdentifier();
+		id.setValue(value);
+		return id;
+	}
+	
+	private static NodeReference buildNodeReference(String value) {
+		NodeReference nodeRef = new NodeReference();
+		nodeRef.setValue(value);
+		return nodeRef;
+	}
+	
+	
+}
