27fc774c5f9432bc4f3e0b32495b3101ebb605d1
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2015-02-04 17:23:25 +0000

    add "views" resource for V2 view API

--- src/main/java/org/dataone/service/util/Constants.java
+++ src/main/java/org/dataone/service/util/Constants.java
@@ -56,6 +56,9 @@ public class Constants {
     public static final String RESOURCE_ARCHIVE = "archive";
     /** Common API resource which handles query operations */
     public static final String RESOURCE_QUERY = "query";
+    
+    /** Common API resource which handles view operations */
+    public static final String RESOURCE_VIEWS = "views";
 
     
     /** AUTHORIZATION API resource which handles access operations */
